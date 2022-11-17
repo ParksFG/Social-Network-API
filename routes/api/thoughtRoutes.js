175ad345b10e5d3b55bdb442189b60509946c4c5
ParksFG <ParkerFGay@gmail.com>
2022-11-16 23:14:17 -0600

    Code cleanup

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,9 +9,16 @@ const {
     deleteReactionByThoughtId
 } = require('../../controllers/thoughtController');
 
+// localhost:PORT/api/thoughts
 router.route('/').get(getAllThoughts).post(postNewThought);
+
+// localhost:PORT/api/thoughts/thoughtId
 router.route('/:thoughtId').get(getThoughtById).put(updateThoughtById).delete(deleteThoughtById);
+
+// localhost:3001/api/thoughts/thoughtId/reactions
 router.route('/:thoughtId/reactions').post(postReactionByThoughtId);
+
+// localhost:3001/api/thoughts/thoughtId/reactions/reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReactionByThoughtId);
 
 module.exports = router;
