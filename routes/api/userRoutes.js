db9e05ab81abab7ac0e1d0684bf99d65a185614e
ParksFG <ParkerFGay@gmail.com>
2022-11-16 23:15:12 -0600

    Added comments for easier reading

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,8 +9,13 @@ const {
     deleteFriend,
 } = require('../../controllers/userController')
 
+// localhost:3001/api/users
 router.route('/').get(getAllUsers).post(postNewUser);
+
+// localhost:3001/api/users/userId
 router.route('/:userId').get(getUserByID).put(updateUserById).delete(deleteUserById);
+
+// localhost:3001/api/users/userId/friends/friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
